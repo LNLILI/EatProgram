305450fef3a92cea9aa76fe01f9e23ef598931d8
Administrator <Administrator@kz201804030937>
2018-08-29 16:09:52 +0800

    hahahaha

--- eat/src/view/AddUserForm.java
+++ eat/src/view/AddUserForm.java
@@ -8,8 +8,16 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
+
+import dao.impl.caipinDaoimpl;
+import entity.User;
+import util.DBUTil;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 	public class AddUserForm extends JInternalFrame {
 		private JTextField textField;
@@ -47,23 +55,32 @@ import java.awt.event.ActionEvent;
 			lblNewLabel.setBounds(83, 43, 42, 15);
 			getContentPane().add(lblNewLabel);
 			
-			JLabel label = new JLabel("\u5BC6\u7801:");
-			label.setBounds(83, 80, 37, 15);
+			JLabel label = new JLabel("\u771F\u5B9E\u59D3\u540D");
+			label.setBounds(83, 80, 79, 15);
 			getContentPane().add(label);
 			
-			JLabel label_1 = new JLabel("\u786E\u8BA4\u5BC6\u7801:");
+			JLabel label_1 = new JLabel("\u5BC6\u7801");
 			label_1.setBounds(83, 117, 79, 15);
 			getContentPane().add(label_1);
 			
-			JLabel label_2 = new JLabel("\u771F\u5B9E\u59D3\u540D:");
-			label_2.setBounds(83, 157, 79, 15);
-			getContentPane().add(label_2);
-			
 			JLabel label_3 = new JLabel("\u6027\u522B:");
-			label_3.setBounds(83, 196, 30, 15);
+			label_3.setBounds(83, 155, 30, 15);
 			getContentPane().add(label_3);
 			
 			JButton button = new JButton("\u6CE8\u518C");
+			button.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					User user=new User();
+					user.setUsername(textField.getText());
+					user.setRealname(textField_1.getText());
+					user.setPassword(textField_2.getText());
+					user.setGender(textField_3.getText());
+					String sql = "insert into user values (?,?,?,?,null)";
+					int a=DBUTil.executeUpdate(sql,user.getUsername(),user.getRealname(),user.getPassword(),user.getGender());
+					System.out.println("成功,影响行数："+a);
+				}
+			});
 			button.setBounds(114, 259, 77, 23);
 			getContentPane().add(button);
 			
@@ -74,14 +91,6 @@ import java.awt.event.ActionEvent;
 			});
 			button_1.setBounds(222, 259, 77, 23);
 			getContentPane().add(button_1);
-			
-			JRadioButton rdbtnNewRadioButton = new JRadioButton("\u7537");
-			rdbtnNewRadioButton.setBounds(161, 192, 54, 23);
-			getContentPane().add(rdbtnNewRadioButton);
-			
-			JRadioButton radioButton = new JRadioButton("\u5973");
-			radioButton.setBounds(222, 192, 54, 23);
-			getContentPane().add(radioButton);
 			
 			textField = new JTextField();
 			textField.setBounds(172, 40, 140, 21);
@@ -99,8 +108,12 @@ import java.awt.event.ActionEvent;
 			getContentPane().add(textField_2);
 			
 			textField_3 = new JTextField();
-			textField_3.setColumns(10);
-			textField_3.setBounds(172, 154, 140, 21);
+			textField_3.setBounds(172, 152, 42, 21);
 			getContentPane().add(textField_3);
+			textField_3.setColumns(10);
+			
+			JLabel label_2 = new JLabel("(\u8BF7\u8F93\u5165\u7537\u6216\u8005\u5973)");
+			label_2.setBounds(222, 155, 108, 15);
+			getContentPane().add(label_2);
 		}
 	}
